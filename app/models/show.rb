e26901a1f1d3a46c6ea8508f0df3ee8c9d0c5432
mireie <michael.reiersgaard@gmail.com>
2021-09-24 16:14:52 -0700

    add date scopes

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,6 @@
 class Show < ApplicationRecord
   belongs_to :venue
   has_one_attached :show_img
+  scope :upcoming, -> { where('showtime <?', Time.now) }
+  scope :past, -> { where.not('showtime <?', Time.now) }
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,5 @@
 class Venue < ApplicationRecord
   has_many :shows
   has_one_attached :venue_img
+  
 end
