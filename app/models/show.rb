7a1d51a65bf9c7bb812c30d0a4edd8d12b1d3704
mireie <michael.reiersgaard@gmail.com>
2021-09-24 18:25:17 -0700

    add get_artists method

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,10 @@ class Show < ApplicationRecord
   has_one_attached :show_img
   scope :upcoming, -> { where('showtime >?', Time.now) }
   scope :past, -> { where.not('showtime >?', Time.now) }
+
+  def get_artists
+    artists = [self.artist1, self.artist2, self.artist3, self.artist4]
+    artists.reject!(&:empty?)
+    artists.join(", ")
+  end
 end
