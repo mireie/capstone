3cd6f34c2a2cbb2b6a30a674688fcc5e829f1c64
mireie <michael.reiersgaard@gmail.com>
2021-10-07 12:57:57 -0700

    add homepage cards when venue selected

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,7 +3,7 @@ class ShowsController < ApplicationController
 
   # GET /shows or /shows.json
   def index
-    @shows = Show.order(:showtime).page(params[:page]).includes([:show_img_attachment])
+    @shows = Show.order(:showtime).page(params[:page])
   end
 
   # GET /shows/1 or /shows/1.json
